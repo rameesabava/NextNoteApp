d5fbe3c184773d56e57651e3652b8ec60397dc13
rameesabava <rameesabava91@gmail.com>
2026-05-21 10:58:35 +0530

    Initial commit

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function UpdateNote(){
+    return(
+        <div>Update note</div>
+    )
+}
---
+++ app/notes/add/page.tsx
@@ -0,0 +1,5 @@
+export default function AddNote(){
+    return(
+        <div>Add note</div>
+    )
+}
